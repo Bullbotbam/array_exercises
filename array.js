302feece1844a03be1186b419ff5b963ae31c306
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2023-03-30 12:35:27 -0500

    first commit

---
+++ array.js
@@ -0,0 +1,13 @@
+let people = ["Greg", "Kevin", "Ray", "James"]
+
+// Using a loop, iterate through this array and console.log all of the people.
+// Write the command to remove "Greg" from the array.
+// Write the command to remove "James" from the array.
+// Write the command to add "Matt" to the front of the array.
+// Write the command to add your name to the end of the array.
+// Using a loop, iterate through this array and after console.log-ing "Kevin", exit from the loop.
+// Write the command to make a copy of the array using slice. The copy should NOT include "Kevin" or "Matt".
+// Write the command that gives the indexOf where "Kevin" is located.
+// Write the command that gives the indexOf where "Foo" is located (this should return -1).
+// Redefine the people variable with the value you started with. Using the splice command, remove "Ray" from the array and add "Elizabeth" and "Artie". Your array should look like this when you are done ["Greg", "Kevin", "Elizabeth", "Artie", "James"].
+// Create a new variable called withBob and set it equal to the people array concatenated with the string of "Bob".
